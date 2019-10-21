789089d5aec5c8478b4929c0848a349c16d85cda
DomitriusAnthony <domitriusaclark@gmail.com>
2019-10-20 21:40:48 -0400

    Added currentUser query to schema and fixed logout mispelling.

--- server/apollo/resolvers.js
+++ server/apollo/resolvers.js
@@ -1,22 +1,20 @@
 const resolvers = {
-    Query: {
-        /*
-            We're going to want to add a way to query our current user
-        */
-    },
-    Mutation: {
-        /*
-            We're going to need to build out a few resolvers to support our authentication flow. We will be tapping into methods created by prisma
-            to deal with storing data and tapping into the mutations created from our datamodel. 
-            
-            1. register
-            2. login
-            3. logout
-        */
-        register: async () => { },
-        login: async () => { },
-        logouy: () => { }
-    }
+  Query: {
+    currentUser: () => { }
+  },
+  Mutation: {
+    /*
+        We're going to need to build out a few resolvers to support our authentication flow. We will be tapping into methods created by prisma
+        to deal with storing data and tapping into the mutations created from our datamodel. 
+        
+        1. register
+        2. login
+        3. logout
+    */
+    register: async () => { },
+    login: async () => { },
+    logout: () => { }
+  }
 }
 
 module.exports = resolvers;
